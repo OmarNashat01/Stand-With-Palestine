62ea96d11568c721425789a3cdada51d823e4c84
Yahia Zakaria <yahiazakaria3200@gmail.com>
2023-10-29 16:40:58 +0300

    feat: add navbar and hero components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Navbar from "./components/navbar/navbar";
+
 import { Inter } from "next/font/google";
 import "./globals.scss";
 
@@ -9,10 +11,17 @@ export const metadata = {
     "This website is for documenting high-precision information about the Israeli aggression against the Palestinian people since the beginning of the occupation in 1948.",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import Hero from "./components/hero/hero";
+
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <Hero
+        title={"How long will the lies and aggression continue?"}
+        imgPath={"/images/hero.png"}
+      />
+    </main>
+  );
 }

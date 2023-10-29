7c6f4f096fbcdbf3efd299d922287146ba05c3f8
HazemAbdo <hazemabdoabdrbo@gmail.com>
2023-10-29 13:26:02 +0200

    refactor(whole project): adding typescript support

---
+++ app/components/timelinesection/TimelineSection.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+/* 
+content
+*/
+const TimelineSection = ({direction,images,date,content}) => {
+  return <div>TimelineSection</div>;
+};
+
+export default TimelineSection;

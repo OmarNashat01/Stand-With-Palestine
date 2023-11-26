9fc32778a5eda5eb5b1365e0afb719816c58a640
Essam <essamwisam@outlook.com>
2023-11-25 21:40:41 -0600

    ✨ UI Retouches and Non-numeric ID

--- src/Pages/BlogsPage/BlogsListPage.tsx
+++ src/Pages/BlogsPage/BlogsListPage.tsx
@@ -1,38 +1,45 @@
 import React from "react";
 import { blogs } from "./BlogsPageData";
 import Card from "../../components/Simple/Card";
+import Banner from "../../components/Simple/Banner";
 
 const BlogsListPage: React.FC = () => {
-    return (
-        <div
-            style={{
-                flex: 1,
-                borderTopLeftRadius: "4rem",
-                borderTopRightRadius: "4rem",
-                backgroundColor: "#1d1d1d",
-                padding: "4rem",
-            }}
-        >
-            <div
-                style={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                    justifyContent: "center",
-                    gap: "3rem",
-                }}
-            >
-                {Object.values(blogs).map((blog) => (
-                    <Card
-                        key={`blog-${blog.id}`}
-                        imagePath={blog.imagePath}
-                        title={blog.name}
-                        summary={blog.summary}
-                        link={`/blogs/${blog.id}`}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+  const visibleBlogs = blogs.filter((blog) => blog.showBlog);
+  return (
+    <div
+      style={{
+        flex: 1,
+        backgroundColor: "#141414",
+      }}
+    >
+      <Banner
+        title="Read Blogs and Interviews Regarding the Ongoing Conflict"
+        styleObj={{ color: "white" }}
+      />
+      <div
+        style={{
+          marginTop: "2rem",
+          paddingTop: "2rem",
+          paddingBottom: "3rem",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          gap: "1.5rem",
+        }}
+      >
+        {Object.values(visibleBlogs).map((blog) => (
+          <Card
+            key={`blog-${blog.blogPath}`}
+            imagePath={blog.imagePath}
+            title={blog.name}
+            summary={blog.summary}
+            //@ts-ignore
+            link={`/blogs/${(blog.blogPath).match(/\/blogs\/(.*).md/)[1]}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BlogsListPage;
--- src/Pages/BlogsPage/BlogsPageData.ts
+++ src/Pages/BlogsPage/BlogsPageData.ts
@@ -1,50 +1,58 @@
 interface Blog {
-    id: number;
+    showBlog: boolean;
     name: string;
+    subTitle: string;
     imagePath: string;
     blogPath: string;
     summary: string;
 }
 
-export const blogs: Record<string, Blog> = {
-    "1": {
-        id: 1,
+export const blogs = [
+    {
+        showBlog: true,
         name: "Very Important Blog to Read!",
+        subTitle: "A Blog For the Next Gen Z Kids",
         imagePath: "/Gaza-Now.jpeg",
         blogPath: "/blogs/mock-blog.md",
         summary:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a tempor nunc. Vestibulum id ligula.",
     },
-    "2": {
-        id: 2,
+    {
+        showBlog: false,
         name: "Blog 2",
+        subTitle: "A Blog For the Next Gen Z Kids",
         imagePath: "/Gaza-red.jpeg",
         blogPath: "/blogs/mock-blog.md",
         summary:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a tempor nunc. Vestibulum id ligula.",
     },
-    "3": {
-        id: 3,
+    {
+        showBlog: true,
         name: "Blog 3",
+        subTitle: "A Blog For the Next Gen Z Kids",
         imagePath: "/Gaza-Now2.jpeg",
         blogPath: "/blogs/mock-blog.md",
         summary:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a tempor nunc. Vestibulum id ligula.",
     },
-    "4": {
+    {
+        showBlog: true,
         id: 4,
         name: "Blog 4",
         imagePath: "/Gaza-Sat.jpeg",
+        subTitle: "A Blog For the Next Gen Z Kids",
         blogPath: "/blogs/mock-blog.md",
         summary:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a tempor nunc. Vestibulum id ligula.",
     },
-    "5": {
+    {
+        showBlog: true,
         id: 5,
         name: "Blog 5",
+        subTitle: "A Blog For the Next Gen Z Kids",
         imagePath: "/Gaza-smoke.jpeg",
         blogPath: "/blogs/mock-blog.md",
         summary:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin purus sapien, semper eget tincidunt vel, interdum sit amet erat. Duis.",
     },
-};
+];

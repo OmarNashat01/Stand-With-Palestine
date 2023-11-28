f554bb937c3449d356f1643ac7e8b2d811ee5f21
Ahmed Khaled <kahmd1444@gmail.com>
2023-11-28 02:25:10 +0200

    Support Page

--- src/Pages/SupportPage/SupportPage.tsx
+++ src/Pages/SupportPage/SupportPage.tsx
@@ -3,6 +3,8 @@ import Card from "../../components/Simple/Card";
 import { supportPageData } from '../../PagesData/SupportPageData'
 import OrgCard from "../../components/donations/orgCard";
 import { Carousel } from "react-responsive-carousel";
+import HeroButton from "../../components/Simple/HeroButton";
+import Banner from "../../components/Simple/Banner";
 
 const SupportPage: React.FC = () => {
   const [selectedOrg, setSelectedOrg] = React.useState(0);
@@ -15,6 +17,7 @@ const SupportPage: React.FC = () => {
         backgroundColor: "#101010",
       }}
     >
+      <Banner title="Stand With Palestine" />
       <div
         style={{
           marginTop: "2rem",
@@ -42,6 +45,104 @@ const SupportPage: React.FC = () => {
 
       </div>
 
+      {/* Selected Org Detailes Section */}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          gap: "1rem",
+          alignItems: "center",
+          flexWrap: "revert",
+          padding: "1rem",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            gap: "1rem",
+            alignItems: "center",
+            flexWrap: "wrap",
+            padding: "1rem",
+          }}
+        >
+          <Carousel showThumbs={false} showStatus={false} showIndicators={true} showArrows={true} autoPlay={true} infiniteLoop={true} interval={3000} stopOnHover={true} >
+            {supportPageData[selectedOrg].trucksImages.map((item, index) => {
+              return (
+                <div key={index} style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
+                  <img
+                    src={item}
+                    style={{ width: "100%", height: "30rem" }}
+
+                  />
+                </div>
+              )
+            })}
+          </Carousel>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "start",
+            gap: "1rem",
+            alignItems: "start",
+            flexWrap: "wrap",
+            padding: "1rem",
+            maxWidth: "40%"
+          }}
+        >
+          <div style={{ display: "flex", flexDirection: "row", gap: "1rem", flexWrap: "wrap" }}>
+
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                gap: "1rem",
+                alignItems: "center",
+                flexWrap: "wrap",
+                padding: "1rem",
+              }}
+            >
+              <img
+                src={supportPageData[selectedOrg].logo}
+                style={{ width: "10rem", height: "10rem" }}
+              />
+              <p
+                style={{
+                  color: "white",
+                  fontSize: "1.5rem",
+                  fontWeight: "bold",
+                }}
+              >
+                {supportPageData[selectedOrg].organization}
+              </p>
+
+              {/* Hero Button */}
+              <div style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
+                <HeroButton
+                  type="blue"
+                  content="Support ♥"
+                  fontSize="1.2rem"
+                  onClickFunc={() => window.open(supportPageData[selectedOrg].donationLink, "_blank")}
+                />
+                <HeroButton
+                  type="red"
+                  content="Learn More"
+                  fontSize="1.2rem"
+                  onClickFunc={() => window.open(supportPageData[selectedOrg].learnMoreLink, "_blank")}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
     </div>
   );
 };
--- src/PagesData/SupportPageData.tsx
+++ src/PagesData/SupportPageData.tsx
@@ -4,69 +4,110 @@ export const supportPageData = [
         "donationLink": "https://donate.unrwa.org/-landing-page/en_EN",
         "learnMoreLink": "https://www.unrwa.org/",
         "logo": "./SupportPage/UnrwaLogo.png",
-        "trucksImages": []
+        "trucksImages": [
+            "./SupportPage/Unrwa-1.jpg",
+            "./SupportPage/Unrwa-2.jpg",
+            "./SupportPage/Unrwa-3.jpg",
+            "./SupportPage/Unrwa-4.jpg",
+            "./SupportPage/Unrwa-5.jpg"
+        ]
     },
     {
         "organization": "Unicef",
         "donationLink": "https://help.unicef.org/eg/donate-to-children",
         "learnMoreLink": "https://www.unicef.org/",
         "logo": "./SupportPage/UnicefLogo.png",
-        "trucksImages": []
+        "trucksImages": [
+            "./SupportPage/Unicef-1.jpg",
+            "./SupportPage/Unicef-2.jpg",
+            "./SupportPage/Unicef-3.jpg",
+            "./SupportPage/Unicef-4.jpg",
+            "./SupportPage/Unicef-5.jpg"
+
+        ]
     },
     {
         "organization": "World Food Programme",
         "donationLink": "https://donate.wfp.org/1244/donation/single/?campaign=3493",
         "learnMoreLink": "https://www.wfp.org/",
         "logo": "./SupportPage/WorldFoodProgrammeLogo.png",
-        "trucksImages": []
+        "trucksImages": [
+            "./SupportPage/WFP-1.jpg",
+            "./SupportPage/WFP-2.jpg",
+            "./SupportPage/WFP-3.jpg",
+        ]
     },
     {
         "organization": "International Committee of the Red Cross",
         "donationLink": "https://www.icrc.org/en/donate/gaza-is-powerless",
         "learnMoreLink": "https://www.icrc.org/",
         "logo": "./SupportPage/RedCrossLogo.png",
-        "trucksImages": []
+        "trucksImages": [
+            "./SupportPage/ICRC-1.jpg",
+        ]
     },
-    {
-        "organization": "Palestinian Red Crescent",
-        "donationLink": "https://www.palestinercs.org/ar/donation",
-        "learnMoreLink": "https://www.palestinercs.org/en",
-        "logo": "./SupportPage/PalestinianRedCrescentLogo.png",
-        "trucksImages": []
-    },
+
     {
         "organization": "Egyptian Red Crescent",
         "donationLink": "https://www.egyptianrc.org/Donate/online-donation",
         "learnMoreLink": "https://www.egyptianrc.org/",
         "logo": "./SupportPage/EgyptianRedCrescentLogo.png",
-        "trucksImages": []
+        "trucksImages": [
+            "./SupportPage/ERC-1.jpg",
+            "./SupportPage/ERC-2.jpeg",
+            "./SupportPage/ERC-3.jpg",
+            "./SupportPage/ERC-4.jpg",
+        ]
     },
     {
         "organization": "Kuwaiti Red Crescent",
         "donationLink": "https://www.krcs.org.kw/Donation/Relief-Palestine",
         "learnMoreLink": "https://www.krcs.org.kw/",
         "logo": "./SupportPage/KuwaitiRedCrescentLogo.png",
-        "trucksImages": []
+        "trucksImages": [
+            "./SupportPage/KRC-1.jpg",
+            "./SupportPage/KRC-2.png",
+            "./SupportPage/KRC-3.jpg",
+        ]
     },
     {
         "organization": "Egyptian Food Bank",
         "donationLink": "https://www.efb.eg/programs/3075/aghath-aaagl-lahaly-ghz?gad_source=1",
         "learnMoreLink": "https://www.efb.eg/",
-        "logo": "./SupportPage/EgyptianFoodBankLogo.png",
-        "trucksImages": []
+        "logo": "./SupportPage/EgyptianFoodBankLogo.jpg",
+        "trucksImages": [
+            "./SupportPage/EFB-1.jpg",
+            "./SupportPage/EFB-2.jpg",
+            "./SupportPage/EFB-3.jpg"
+        ]
     },
     {
         "organization": "Mersal",
         "donationLink": "https://www.mersal-ngo.org/Donation",
         "learnMoreLink": "https://www.mersal-ngo.org/",
-        "logo": "./SupportPage/MersalLogo.png",
-        "trucksImages": []
+        "logo": "./SupportPage/MersalLogo.jpg",
+        "trucksImages": [
+            "./SupportPage/Mersal-1.jpeg",
+            "./SupportPage/Mersal-2.jpg",
+            "./SupportPage/Mersal-3.jpg",
+            "./SupportPage/Mersal-4.jpg",
+        ]
     },
     {
         "organization": "Zakat Foundation of America",
         "donationLink": "https://secure.zakat.org/np/clients/zakat/donation.jsp?customDataBean.78.optionIds=41",
         "learnMoreLink": "https://www.zakat.org/",
         "logo": "./SupportPage/ZakatFoundationLogo.png",
+        "trucksImages": [
+            "./SupportPage/ZFA-1.jpg",
+            "./SupportPage/ZFA-2.jpg",
+        ]
+    },
+    {
+        "organization": "Palestinian Red Crescent",
+        "donationLink": "https://www.palestinercs.org/ar/donation",
+        "learnMoreLink": "https://www.palestinercs.org/en",
+        "logo": "./SupportPage/PalestinianRedCrescentLogo.png",
         "trucksImages": []
     },
     {

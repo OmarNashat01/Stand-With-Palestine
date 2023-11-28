0e4366b6328772ff7161101792d81a1e99475854
Ahmed Khaled <kahmd1444@gmail.com>
2023-11-28 04:13:50 +0200

    Support Page Add Multi Carousel and minor edits

--- src/Pages/SupportPage/SupportPage.tsx
+++ src/Pages/SupportPage/SupportPage.tsx
@@ -2,12 +2,37 @@ import React from "react";
 import Card from "../../components/Simple/Card";
 import { supportPageData } from '../../PagesData/SupportPageData'
 import OrgCard from "../../components/donations/orgCard";
-import { Carousel } from "react-responsive-carousel";
+import { Carousel as ResponsiveCarousel } from "react-responsive-carousel";
+import Carousel from "react-multi-carousel";
+
 import HeroButton from "../../components/Simple/HeroButton";
 import Banner from "../../components/Simple/Banner";
 
+import "react-multi-carousel/lib/styles.css";
+
 const SupportPage: React.FC = () => {
   const [selectedOrg, setSelectedOrg] = React.useState(0);
+
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+      slidesToSlide: 4 // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2 // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1 // optional, default to 1.
+    }
+  };
+
+
+
   return (
     <div
       style={{
@@ -18,31 +43,36 @@ const SupportPage: React.FC = () => {
       }}
     >
       <Banner title="Stand With Palestine" />
-      <div
-        style={{
-          marginTop: "2rem",
-          paddingTop: "2rem",
-          paddingBottom: "3rem",
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "center",
-          gap: "1rem",
-        }}
-      >
-        {/* Horizontal Grid Section */}
-        {supportPageData.map((item, index) => {
-          return (
-            <OrgCard
-              key={index}
-              title={item.organization}
-              link={item.learnMoreLink}
-              selected={selectedOrg === index}
-              onClick={() => setSelectedOrg(index)}
-            />
-          );
-        }
-        )}
 
+      <div style={{ paddingBottom: "2%", paddingTop: "2%" }}>
+        {/* Horizontal Grid Section */}
+        <Carousel
+          swipeable={false}
+          draggable={false}
+          showDots={true}
+          responsive={responsive}
+          ssr={true}
+          infinite={true}
+          autoPlaySpeed={1000}
+          keyBoardControl={true}
+          customTransition="all .5"
+          transitionDuration={500}
+          containerClass="carousel-container"
+          removeArrowOnDeviceType={["tablet", "mobile"]}
+          dotListClass="custom-dot-list-style"
+          itemClass="carousel-item-padding-40-px" >
+          {supportPageData.map((item, index) => {
+            return (
+              <OrgCard
+                // key={index}
+                title={item.organization}
+                description={item.description}
+                selected={index === selectedOrg}
+                onClick={() => setSelectedOrg(index)}
+              />
+            );
+          })}
+        </Carousel>
       </div>
 
       {/* Selected Org Detailes Section */}
@@ -66,9 +96,10 @@ const SupportPage: React.FC = () => {
             alignItems: "center",
             flexWrap: "wrap",
             padding: "1rem",
+            maxWidth: "60%"
           }}
         >
-          <Carousel showThumbs={false} showStatus={false} showIndicators={true} showArrows={true} autoPlay={true} infiniteLoop={true} interval={3000} stopOnHover={true} >
+          <ResponsiveCarousel showThumbs={false} showStatus={false} showIndicators={true} showArrows={true} autoPlay={true} infiniteLoop={true} interval={3000} stopOnHover={true} >
             {supportPageData[selectedOrg].trucksImages.map((item, index) => {
               return (
                 <div key={index} style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
@@ -80,7 +111,7 @@ const SupportPage: React.FC = () => {
                 </div>
               )
             })}
-          </Carousel>
+          </ResponsiveCarousel>
         </div>
 
         <div
@@ -108,32 +139,36 @@ const SupportPage: React.FC = () => {
                 padding: "1rem",
               }}
             >
-              <img
-                src={supportPageData[selectedOrg].logo}
-                style={{ width: "10rem", height: "10rem" }}
-              />
               <p
                 style={{
                   color: "white",
-                  fontSize: "1.5rem",
+                  fontSize: "3rem",
                   fontWeight: "bold",
                 }}
               >
                 {supportPageData[selectedOrg].organization}
               </p>
-
+              <p
+                style={{
+                  color: "white",
+                  fontSize: "1rem",
+                  fontWeight: "bold",
+                }}
+              >
+                {supportPageData[selectedOrg].description}{`So far they sent ${supportPageData[selectedOrg].trucksNumber} trucks`}
+              </p>
               {/* Hero Button */}
               <div style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
                 <HeroButton
                   type="blue"
                   content="Support ♥"
-                  fontSize="1.2rem"
+                  fontSize=".8rem"
                   onClickFunc={() => window.open(supportPageData[selectedOrg].donationLink, "_blank")}
                 />
                 <HeroButton
                   type="red"
                   content="Learn More"
-                  fontSize="1.2rem"
+                  fontSize=".8rem"
                   onClickFunc={() => window.open(supportPageData[selectedOrg].learnMoreLink, "_blank")}
                 />
               </div>
@@ -143,7 +178,7 @@ const SupportPage: React.FC = () => {
 
 
       </div>
-    </div>
+    </div >
   );
 };
 
--- src/PagesData/SupportPageData.tsx
+++ src/PagesData/SupportPageData.tsx
@@ -1,10 +1,13 @@
 export const supportPageData = [
     {
         "organization": "Unrwa",
+        "description": "The United Nations Relief and Works Agency for Palestine Refugees (UNRWA) is a humanitarian organization providing assistance and protection to registered Palestine refugees.",
+        "trucksNumber": "X",
         "donationLink": "https://donate.unrwa.org/-landing-page/en_EN",
         "learnMoreLink": "https://www.unrwa.org/",
         "logo": "./SupportPage/UnrwaLogo.png",
         "trucksImages": [
+            "./SupportPage/UnrwaLogo.png",
             "./SupportPage/Unrwa-1.jpg",
             "./SupportPage/Unrwa-2.jpg",
             "./SupportPage/Unrwa-3.jpg",
@@ -14,10 +17,13 @@ export const supportPageData = [
     },
     {
         "organization": "Unicef",
+        "description": "UNICEF works in over 190 countries and territories to save children's lives, to defend their rights, and to help them fulfil their potential, from early childhood through adolescence.",
+        "trucksNumber": "X",
         "donationLink": "https://help.unicef.org/eg/donate-to-children",
         "learnMoreLink": "https://www.unicef.org/",
         "logo": "./SupportPage/UnicefLogo.png",
         "trucksImages": [
+            "./SupportPage/UnicefLogo.png",
             "./SupportPage/Unicef-1.jpg",
             "./SupportPage/Unicef-2.jpg",
             "./SupportPage/Unicef-3.jpg",
@@ -28,10 +34,13 @@ export const supportPageData = [
     },
     {
         "organization": "World Food Programme",
+        "description": "The World Food Programme is the food-assistance branch of the United Nations and the world's largest humanitarian organization addressing hunger and promoting food security.",
+        "trucksNumber": "X",
         "donationLink": "https://donate.wfp.org/1244/donation/single/?campaign=3493",
         "learnMoreLink": "https://www.wfp.org/",
         "logo": "./SupportPage/WorldFoodProgrammeLogo.png",
         "trucksImages": [
+            "./SupportPage/WorldFoodProgrammeLogo.png",
             "./SupportPage/WFP-1.jpg",
             "./SupportPage/WFP-2.jpg",
             "./SupportPage/WFP-3.jpg",
@@ -39,20 +48,26 @@ export const supportPageData = [
     },
     {
         "organization": "International Committee of the Red Cross",
+        "description": "The ICRC operates worldwide, helping people affected by conflict and armed violence and promoting the laws that protect victims of war.",
+        "trucksNumber": "X",
         "donationLink": "https://www.icrc.org/en/donate/gaza-is-powerless",
         "learnMoreLink": "https://www.icrc.org/",
         "logo": "./SupportPage/RedCrossLogo.png",
         "trucksImages": [
+            "./SupportPage/RedCrossLogo.png",
             "./SupportPage/ICRC-1.jpg",
         ]
     },
 
     {
         "organization": "Egyptian Red Crescent",
+        "description": "The Egyptian Red Crescent is a humanitarian organization that provides emergency assistance, disaster relief and education inside Egypt and internationally.",
+        "trucksNumber": "X",
         "donationLink": "https://www.egyptianrc.org/Donate/online-donation",
         "learnMoreLink": "https://www.egyptianrc.org/",
         "logo": "./SupportPage/EgyptianRedCrescentLogo.png",
         "trucksImages": [
+            "./SupportPage/EgyptianRedCrescentLogo.png",
             "./SupportPage/ERC-1.jpg",
             "./SupportPage/ERC-2.jpeg",
             "./SupportPage/ERC-3.jpg",
@@ -61,10 +76,13 @@ export const supportPageData = [
     },
     {
         "organization": "Kuwaiti Red Crescent",
+        "description": "The Kuwait Red Crescent Society is a voluntary humanitarian society that provides assistance to the most vulnerable groups in Kuwait and abroad.",
+        "trucksNumber": "X",
         "donationLink": "https://www.krcs.org.kw/Donation/Relief-Palestine",
         "learnMoreLink": "https://www.krcs.org.kw/",
         "logo": "./SupportPage/KuwaitiRedCrescentLogo.png",
         "trucksImages": [
+            "./SupportPage/KuwaitiRedCrescentLogo.png",
             "./SupportPage/KRC-1.jpg",
             "./SupportPage/KRC-2.png",
             "./SupportPage/KRC-3.jpg",
@@ -72,10 +90,13 @@ export const supportPageData = [
     },
     {
         "organization": "Egyptian Food Bank",
+        "description": "The Egyptian Food Bank is a non-profit organization that aims to secure healthy nutritious Food To alleviate the suffering Of hunger",
+        "trucksNumber": "X",
         "donationLink": "https://www.efb.eg/programs/3075/aghath-aaagl-lahaly-ghz?gad_source=1",
         "learnMoreLink": "https://www.efb.eg/",
         "logo": "./SupportPage/EgyptianFoodBankLogo.jpg",
         "trucksImages": [
+            "./SupportPage/EgyptianFoodBankLogo.jpg",
             "./SupportPage/EFB-1.jpg",
             "./SupportPage/EFB-2.jpg",
             "./SupportPage/EFB-3.jpg"
@@ -83,10 +104,13 @@ export const supportPageData = [
     },
     {
         "organization": "Mersal",
+        "description": "Mersal is a non-profit organization that aims to provide medical care to the most vulnerable groups in Egypt.",
+        "trucksNumber": "X",
         "donationLink": "https://www.mersal-ngo.org/Donation",
         "learnMoreLink": "https://www.mersal-ngo.org/",
         "logo": "./SupportPage/MersalLogo.jpg",
         "trucksImages": [
+            "./SupportPage/MersalLogo.jpg",
             "./SupportPage/Mersal-1.jpeg",
             "./SupportPage/Mersal-2.jpg",
             "./SupportPage/Mersal-3.jpg",
@@ -95,26 +119,26 @@ export const supportPageData = [
     },
     {
         "organization": "Zakat Foundation of America",
+        "description": "Zakat Foundation of America is a US-based, Muslim-run charity organization that serves needy communities at home and abroad.",
+        "trucksNumber": "X",
         "donationLink": "https://secure.zakat.org/np/clients/zakat/donation.jsp?customDataBean.78.optionIds=41",
         "learnMoreLink": "https://www.zakat.org/",
         "logo": "./SupportPage/ZakatFoundationLogo.png",
         "trucksImages": [
+            "./SupportPage/ZakatFoundationLogo.png",
             "./SupportPage/ZFA-1.jpg",
             "./SupportPage/ZFA-2.jpg",
         ]
     },
     {
         "organization": "Palestinian Red Crescent",
+        "description": "The Palestine Red Crescent Society (PRCS) is an officially recognized independent Palestinian National Society. It enjoys legal personality and is part of the International Red Cross and Red Crescent Movement.",
+        "trucksNumber": "X",
         "donationLink": "https://www.palestinercs.org/ar/donation",
         "learnMoreLink": "https://www.palestinercs.org/en",
         "logo": "./SupportPage/PalestinianRedCrescentLogo.png",
-        "trucksImages": []
-    },
-    {
-        "organization": "Life Makers",
-        "donationLink": "https://donate.lifemakers.org/Donate/DetailsAr/162",
-        "learnMoreLink": "https://www.lifemakers.org/",
-        "logo": "./SupportPage/LifeMakersLogo.png",
-        "trucksImages": []
+        "trucksImages": [
+            "./SupportPage/PalestinianRedCrescentLogo.png",
+        ]
     }
 ]

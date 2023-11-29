7e50ee3fe33f6ed776657ad10123d5fe7c2d6fdd
Ahmed Khaled <kahmd1444@gmail.com>
2023-11-30 01:50:27 +0200

    add humanitarian_aid_status

--- src/Pages/SupportPage/SupportPage.tsx
+++ src/Pages/SupportPage/SupportPage.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect} from "react";
+import React, { useRef, useEffect } from "react";
 import { supportPageData, supportActionData } from '../../PagesData/SupportPageData'
 import OrgCard from "../../components/donations/orgCard";
 import { Carousel as ResponsiveCarousel } from "react-responsive-carousel";
@@ -22,15 +22,15 @@ const SupportPage: React.FC = () => {
       //@ts-ignore
       let scrollPosition = orgCardsRef.current.scrollWidth * 0.235;
       if (window.innerWidth < 700) {
-      //@ts-ignore
-       scrollPosition = orgCardsRef.current.scrollWidth * 0.345;
+        //@ts-ignore
+        scrollPosition = orgCardsRef.current.scrollWidth * 0.345;
       }
 
       // Set the scrollLeft property to the calculated value
       //@ts-ignore
       orgCardsRef.current.scrollLeft = scrollPosition;
     }
-  }, []); 
+  }, []);
 
   return (
     <div
@@ -42,11 +42,11 @@ const SupportPage: React.FC = () => {
       }}
     >
       <GradientHero title={"SUPPORT PALESTINE"} subTitle1="They Need Your Help" subTitle2="There Are Many Ways We Can Help Make Things Better" special={true} />
-      <Banner title={"You Could Help Prevent Someone From Dying in the Hospital or From Starving to Death"} styleObj={{color: 'white', backgroundColor:'transparent', boxShadow: 'none'}}/>
-      <div style={{ paddingBottom: "2%", paddingTop: "2%" }} className="org-cards" ref={orgCardsRef}>          
-          {supportPageData.map((item, index) => {
-            return (
-              <div className="card-item">
+      <Banner title={"You Could Help Prevent Someone From Dying in the Hospital or From Starving to Death"} styleObj={{ color: 'white', backgroundColor: 'transparent', boxShadow: 'none' }} />
+      <div style={{ paddingBottom: "2%", paddingTop: "2%" }} className="org-cards" ref={orgCardsRef}>
+        {supportPageData.map((item, index) => {
+          return (
+            <div className="card-item">
               <OrgCard
                 // key={index}
                 title={item.organization}
@@ -54,11 +54,11 @@ const SupportPage: React.FC = () => {
                 selected={index === selectedOrg}
                 onClick={() => setSelectedOrg(index)}
               />
-              </div>
-            );
-          })}
+            </div>
+          );
+        })}
       </div>
-        <div className="container">
+      <div className="container">
         <div className="first-child">
           <ResponsiveCarousel className="carousel" showThumbs={false} dynamicHeight={false} showStatus={false} showIndicators={true} showArrows={true} autoPlay={false} infiniteLoop={true} interval={3000} stopOnHover={true}>
             {supportPageData[selectedOrg].trucksImages.map((item, index) => (
@@ -75,21 +75,21 @@ const SupportPage: React.FC = () => {
           <div className="inner-container">
             <div className="inner-child">
               <h1>{supportPageData[selectedOrg].organization}</h1>
-              <p>{supportPageData[selectedOrg].description}{(false)?`So far they sent ${supportPageData[selectedOrg].trucksNumber} trucks`:``}</p>
+              <p>{supportPageData[selectedOrg].description}{supportPageData[selectedOrg].humanitarian_aid_status}</p>
               {/* Buttons */}
               <div className="buttons">
-                <HeroButton type="blue" content="More Info" fontSize="1.0rem" styleObj={{borderRadius:'2rem'}} onClickFunc={() => window.open(supportPageData[selectedOrg].learnMoreLink, "_blank")} />
-                <HeroButton type="blue" content="Support ♥" fontSize="1.0rem" styleObj={{borderRadius:'2rem'}} onClickFunc={() => window.open(supportPageData[selectedOrg].donationLink, "_blank")} />
+                <HeroButton type="blue" content="More Info" fontSize="1.0rem" styleObj={{ borderRadius: '2rem' }} onClickFunc={() => window.open(supportPageData[selectedOrg].learnMoreLink, "_blank")} />
+                <HeroButton type="blue" content="Support ♥" fontSize="1.0rem" styleObj={{ borderRadius: '2rem' }} onClickFunc={() => window.open(supportPageData[selectedOrg].donationLink, "_blank")} />
               </div>
             </div>
           </div>
         </div>
-        </div>
-<Banner title={"Or You Could Help End This Massacre Altogether"} styleObj={{color: 'white', backgroundColor:'transparent', boxShadow: 'none'}}/>
-<div className="org-cards-s" style={{justifyContent: 'center', overflow: 'hidden'}} >          
-          {supportActionData.map((item, index) => {
-            return (
-              <div className="card-item" >
+      </div>
+      <Banner title={"Or You Could Help End This Massacre Altogether"} styleObj={{ color: 'white', backgroundColor: 'transparent', boxShadow: 'none' }} />
+      <div className="org-cards-s" style={{ justifyContent: 'center', overflow: 'hidden' }} >
+        {supportActionData.map((item, index) => {
+          return (
+            <div className="card-item" >
               <OrgCard
                 small={true}
                 title={item.organization}
@@ -97,12 +97,12 @@ const SupportPage: React.FC = () => {
                 selected={index === selectedOption}
                 onClick={() => setSelectedOption(index)}
               />
-              </div>
-            );
-          })}
+            </div>
+          );
+        })}
       </div>
-      <div className="container"style={{ maxWidth: '100%', margin: 'auto', flexDirection: 'row-reverse'}}>
-        <div className="first-child" style={{flexBasis: '60%'}}>
+      <div className="container" style={{ maxWidth: '100%', margin: 'auto', flexDirection: 'row-reverse' }}>
+        <div className="first-child" style={{ flexBasis: '60%' }}>
           <ResponsiveCarousel className="carousel" showThumbs={false} dynamicHeight={false} showStatus={false} showIndicators={true} showArrows={true} autoPlay={false} infiniteLoop={true} interval={3000} stopOnHover={true}>
             {supportActionData[selectedOption].trucksImages.map((item, index) => (
               <div key={index} className="image-container"  >
@@ -111,18 +111,18 @@ const SupportPage: React.FC = () => {
             ))}
           </ResponsiveCarousel>
         </div>
-        <div className="second-child" style={{flexBasis: '60%'}}>
+        <div className="second-child" style={{ flexBasis: '60%' }}>
           <div className="inner-container">
-            <div className="inner-child" style={{minWidth: '70px'}}>
+            <div className="inner-child" style={{ minWidth: '70px' }}>
               <h1>{supportActionData[selectedOption].organization}</h1>
               <p>{supportActionData[selectedOption].description}</p>
               <div className="buttons">
-                <HeroButton type="blue" content="Act Now ♥" fontSize="1.0rem" styleObj={{borderRadius:'2rem'}} onClickFunc={() => window.open(supportActionData[selectedOption].link, '_blank')} />
+                <HeroButton type="blue" content="Act Now ♥" fontSize="1.0rem" styleObj={{ borderRadius: '2rem' }} onClickFunc={() => window.open(supportActionData[selectedOption].link, '_blank')} />
               </div>
             </div>
           </div>
         </div>
-        </div>
+      </div>
     </div >
   );
 };
--- src/PagesData/SupportPageData.tsx
+++ src/PagesData/SupportPageData.tsx
@@ -3,7 +3,7 @@ export const supportPageData = [
     {
         "organization": "Unicef",
         "description": "UNICEF works in over 190 countries and territories to save children's lives, to defend their rights, and to help them fulfil their potential, from early childhood through adolescence.",
-        "trucksNumber": "X",
+        "humanitarian_aid_status": "They have sent at least 100 trucks into Gaza thus far.",
         "donationLink": "https://help.unicef.org/eg/donate-to-children",
         "learnMoreLink": "https://www.unicef.org/",
         "logo": "./SupportPage/UnicefLogo.png",
@@ -20,7 +20,7 @@ export const supportPageData = [
     {
         "organization": "Unrwa",
         "description": "The United Nations Relief and Works Agency for Palestine Refugees (UNRWA) is a humanitarian organization providing assistance and protection to registered Palestine refugees.",
-        "trucksNumber": "X",
+        "humanitarian_aid_status": "They have sent at least 200 trucks into Gaza thus far.",
         "donationLink": "https://donate.unrwa.org/-landing-page/en_EN",
         "learnMoreLink": "https://www.unrwa.org/",
         "logo": "./SupportPage/UnrwaLogo.png",
@@ -36,7 +36,7 @@ export const supportPageData = [
     {
         "organization": "World Food Programme",
         "description": "The World Food Programme is the food-assistance branch of the United Nations and the world's largest humanitarian organization addressing hunger and promoting food security.",
-        "trucksNumber": "X",
+        "humanitarian_aid_status": "They have sent at least 1,000 metric tons of food line up at the Rafah border thus far.",
         "donationLink": "https://donate.wfp.org/1244/donation/single/?campaign=3493",
         "learnMoreLink": "https://www.wfp.org/",
         "logo": "./SupportPage/WorldFoodProgrammeLogo.png",
@@ -50,7 +50,7 @@ export const supportPageData = [
     {
         "organization": "International Committee of the Red Cross",
         "description": "The ICRC operates worldwide, helping people affected by conflict and armed violence and promoting the laws that protect victims of war.",
-        "trucksNumber": "X",
+        "humanitarian_aid_status": "",
         "donationLink": "https://www.icrc.org/en/donate/gaza-is-powerless",
         "learnMoreLink": "https://www.icrc.org/",
         "logo": "./SupportPage/RedCrossLogo.png",
@@ -62,7 +62,7 @@ export const supportPageData = [
     {
         "organization": "Palestinian Red Crescent",
         "description": "The Palestine Red Crescent Society (PRCS) is an officially recognized independent Palestinian National Society. It enjoys legal personality and is part of the International Red Cross and Red Crescent Movement.",
-        "trucksNumber": "X",
+        "humanitarian_aid_status": "",
         "donationLink": "https://www.palestinercs.org/ar/donation",
         "learnMoreLink": "https://www.palestinercs.org/en",
         "logo": "./SupportPage/prc-logo.png",
@@ -75,7 +75,7 @@ export const supportPageData = [
     {
         "organization": "Egyptian Red Crescent",
         "description": "The Egyptian Red Crescent is a humanitarian organization that provides emergency assistance, disaster relief and education inside Egypt and internationally.",
-        "trucksNumber": "X",
+        "humanitarian_aid_status": "",
         "donationLink": "https://www.egyptianrc.org/Donate/online-donation",
         "learnMoreLink": "https://www.egyptianrc.org/",
         "logo": "./SupportPage/EgyptianRedCrescentLogo.png",
@@ -90,7 +90,7 @@ export const supportPageData = [
     {
         "organization": "Kuwaiti Red Crescent",
         "description": "The Kuwait Red Crescent Society is a voluntary humanitarian society that provides assistance to the most vulnerable groups in Kuwait and abroad.",
-        "trucksNumber": "X",
+        "humanitarian_aid_status": "They have sent at least 1,300 tons included medical and food supplies, tents and chairs into Gaza thus far.",
         "donationLink": "https://www.krcs.org.kw/Donation/Relief-Palestine",
         "learnMoreLink": "https://www.krcs.org.kw/",
         "logo": "./SupportPage/KuwaitiRedCrescentLogo.png",
@@ -103,8 +103,8 @@ export const supportPageData = [
     },
     {
         "organization": "Egyptian Food Bank",
-        "description": "The Egyptian Food Bank is a non-profit organization that aims to secure healthy nutritious Food To alleviate the suffering Of hunger",
-        "trucksNumber": "X",
+        "description": "The Egyptian Food Bank is a non-profit organization that aims to secure healthy nutritious Food To alleviate the suffering Of hunger.",
+        "humanitarian_aid_status": "They have sent at least 307 trucks into Gaza thus far.",
         "donationLink": "https://www.efb.eg/programs/3075/aghath-aaagl-lahaly-ghz?gad_source=1",
         "learnMoreLink": "https://www.efb.eg/",
         "logo": "./SupportPage/EgyptianFoodBankLogo.jpg",
@@ -118,7 +118,7 @@ export const supportPageData = [
     {
         "organization": "Mersal",
         "description": "Mersal is a non-profit organization that aims to provide medical care to the most vulnerable groups in Egypt.",
-        "trucksNumber": "X",
+        "humanitarian_aid_status": "They have sent at least 82 trucks into Gaza thus far.",
         "donationLink": "https://www.mersal-ngo.org/Donation",
         "learnMoreLink": "https://www.mersal-ngo.org/",
         "logo": "./SupportPage/MersalLogo.jpg",
@@ -133,7 +133,7 @@ export const supportPageData = [
     {
         "organization": "Zakat Foundation of America",
         "description": "Zakat Foundation of America is a US-based, Muslim-run charity organization that serves needy communities at home and abroad.",
-        "trucksNumber": "X",
+        "humanitarian_aid_status": "",
         "donationLink": "https://secure.zakat.org/np/clients/zakat/donation.jsp?customDataBean.78.optionIds=41",
         "learnMoreLink": "https://www.zakat.org/",
         "logo": "./SupportPage/ZakatFoundationLogo.png",
